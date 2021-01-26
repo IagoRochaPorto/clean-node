93073d9b735ecb962ce4ac90230072a42b279554
Iago Rocha Porto <iagorochaporto@gmail.com>
2021-01-25 21:56:10 -0300

    refactor: change strict null rule

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -12,8 +12,8 @@ describe('Signup Controller', () => {
       }
     }
     const httpResponse = systemUnderTest.handle(httpRequest)
-    expect(httpResponse?.statusCode).toBe(400)
-    expect(httpResponse?.body).toEqual(new MissingParamError('name'))
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('name'))
   })
 
   test('Should return 400 if no email is provided ', () => {
@@ -26,8 +26,8 @@ describe('Signup Controller', () => {
       }
     }
     const httpResponse = systemUnderTest.handle(httpRequest)
-    expect(httpResponse?.statusCode).toBe(400)
-    expect(httpResponse?.body).toEqual(new MissingParamError('email'))
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
 
   test('Should return 400 if no password is provided ', () => {
@@ -41,7 +41,7 @@ describe('Signup Controller', () => {
     }
     const httpResponse = systemUnderTest.handle(httpRequest)
     expect(httpResponse?.statusCode).toBe(400)
-    expect(httpResponse?.body).toEqual(new MissingParamError('password'))
+    expect(httpResponse.body).toEqual(new MissingParamError('password'))
   })
 
   test('Should return 400 if no password confirmation is provided ', () => {
@@ -54,7 +54,7 @@ describe('Signup Controller', () => {
       }
     }
     const httpResponse = systemUnderTest.handle(httpRequest)
-    expect(httpResponse?.statusCode).toBe(400)
-    expect(httpResponse?.body).toEqual(new MissingParamError('passwordConfirmation'))
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'))
   })
 })
--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -3,7 +3,7 @@ import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
 export class SignupController implements Controller {
-  handle(httpRequest: HttpRequest): HttpResponse | undefined {
+  handle(httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
