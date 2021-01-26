94086fd91376b8d864a32e83bd85e118133ef0e6
Iago Rocha Porto <iagorochaporto@gmail.com>
2021-01-25 21:36:23 -0300

    refactor: ensure SignupController.handle implement correct interfaces

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -11,8 +11,8 @@ describe('Signup Controller', () => {
       }
     }
     const httpResponse = systemUnderTest.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+    expect(httpResponse?.statusCode).toBe(400)
+    expect(httpResponse?.body).toEqual(new Error('Missing param: name'))
   })
 
   test('Should return 400 if no email is provided ', () => {
@@ -25,7 +25,7 @@ describe('Signup Controller', () => {
       }
     }
     const httpResponse = systemUnderTest.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+    expect(httpResponse?.statusCode).toBe(400)
+    expect(httpResponse?.body).toEqual(new Error('Missing param: email'))
   })
 })
