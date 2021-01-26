c4ab9130000f99830ddf4924c7a24121e63b059e
Iago Rocha Porto <iagorochaporto@gmail.com>
2021-01-26 06:29:50 -0300

    refactor: simplify code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -13,15 +13,11 @@ export class SignupController implements Controller {
     try {
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
       for (const field of requiredFields) {
-        if (!httpRequest.body[field]) {
-          return badRequest(new MissingParamError(field))
-        }
+        if (!httpRequest.body[field]) return badRequest(new MissingParamError(field))
       }
 
       const isValid = this.emailValidator.isValid(httpRequest.body.email)
-      if (!isValid) {
-        return badRequest(new InvalidParamError('email'))
-      }
+      if (!isValid) return badRequest(new InvalidParamError('email'))
     } catch (error) {
       return serverError()
     }
