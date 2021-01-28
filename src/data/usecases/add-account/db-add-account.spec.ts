9a0431cefeed3bf2d817bce8b85fa280a760062a
Iago Rocha Porto <iagorochaporto@gmail.com>
2021-01-27 21:15:59 -0300

    refactor: move SystemUnderTest creation to a factory method

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,15 +1,30 @@
 import { DbAddAccount } from './db-add-account'
+import { Encrypter } from '../../protocols/encrypter'
+
+interface SystemUnderTestTypes {
+  systemUnderTest: DbAddAccount
+  encrypterStub: Encrypter
+}
+
+const makeSystemUnderTest = (): SystemUnderTestTypes => {
+  class EncrypterStub {
+    async encrypt(value: string): Promise<string> {
+      return new Promise(resolve => resolve('hashed_password'))
+    }
+  }
+
+  const encrypterStub = new EncrypterStub()
+  const systemUnderTest = new DbAddAccount(encrypterStub)
+
+  return {
+    systemUnderTest,
+    encrypterStub
+  }
+}
 
 describe('DbAddAccount Usecase', () => {
   test('Should call Encrypter with correct password', async () => {
-    class EncrypterStub {
-      async encrypt(value: string): Promise<string> {
-        return new Promise(resolve => resolve('hashed_password'))
-      }
-    }
-
-    const encrypterStub = new EncrypterStub()
-    const systemUnderTest = new DbAddAccount(encrypterStub)
+    const { encrypterStub, systemUnderTest } = makeSystemUnderTest()
     const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
     const accountData = {
       name: 'valid_name',
