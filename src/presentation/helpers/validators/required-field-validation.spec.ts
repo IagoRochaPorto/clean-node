48f7bb7239cd147606af46fb83baa53d6d25795d
Iago Rocha Porto <iagorochaporto@gmail.com>
2021-02-28 15:14:42 -0300

    test: ensure RequiredFieldValidation not return if validation succeeds

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -7,4 +7,10 @@ describe('RequiredField validation', () => {
     const error = systemUnderTest.validate({ name: 'any_name' })
     expect(error).toEqual(new MissingParamError('any_field'))
   })
+
+  test('Should not return if validation fails', () => {
+    const systemUnderTest = new RequiredFieldValidation('any_field')
+    const error = systemUnderTest.validate({ any_field: 'any_field' })
+    expect(error).toBeFalsy()
+  })
 })
