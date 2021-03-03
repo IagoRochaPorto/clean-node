388fa27a2a9b6911f86093dd6f1dd07bba3c716f
Iago Rocha Porto <iagorochaporto@gmail.com>
2021-03-03 19:12:30 -0300

    refactor: rename Encrypter to Hasher

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,33 +1,33 @@
 
 import { DbAddAccount } from './db-add-account'
-import { Encrypter, AccountModel, AddAccountModel, AddAccountRepository } from './db-add-account-protocols'
+import { Hasher, AccountModel, AddAccountModel, AddAccountRepository } from './db-add-account-protocols'
 
 interface SystemUnderTestTypes {
   systemUnderTest: DbAddAccount
-  encrypterStub: Encrypter
+  hasherStub: Hasher
   addAccountRepositoryStub: AddAccountRepository
 }
 
 const makeSystemUnderTest = (): SystemUnderTestTypes => {
-  const encrypterStub = makeEncrypter()
+  const hasherStub = makeHasher()
   const addAccountRepositoryStub = makeAddAccountRepository()
-  const systemUnderTest = new DbAddAccount(encrypterStub, addAccountRepositoryStub)
+  const systemUnderTest = new DbAddAccount(hasherStub, addAccountRepositoryStub)
 
   return {
     systemUnderTest,
-    encrypterStub,
+    hasherStub,
     addAccountRepositoryStub
   }
 }
 
-const makeEncrypter = (): Encrypter => {
-  class EncrypterStub implements Encrypter {
-    async encrypt(value: string): Promise<string> {
+const makeHasher = (): Hasher => {
+  class HasherStub implements Hasher {
+    async hash(value: string): Promise<string> {
       return new Promise(resolve => resolve('hashed_password'))
     }
   }
 
-  return new EncrypterStub()
+  return new HasherStub()
 }
 
 const makeAddAccountRepository = (): AddAccountRepository => {
@@ -54,16 +54,16 @@ const makeFakeAccountData = (): AddAccountModel => ({
 })
 
 describe('DbAddAccount Usecase', () => {
-  test('Should call Encrypter with correct password', async () => {
-    const { encrypterStub, systemUnderTest } = makeSystemUnderTest()
-    const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
+  test('Should call Hasher with correct password', async () => {
+    const { hasherStub, systemUnderTest } = makeSystemUnderTest()
+    const encryptSpy = jest.spyOn(hasherStub, 'hash')
     await systemUnderTest.add(makeFakeAccountData())
     expect(encryptSpy).toHaveBeenCalledWith('valid_password')
   })
 
-  test('Should throw if Encrypter throws', async () => {
-    const { encrypterStub, systemUnderTest } = makeSystemUnderTest()
-    jest.spyOn(encrypterStub, 'encrypt').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+  test('Should throw if Hasher throws', async () => {
+    const { hasherStub, systemUnderTest } = makeSystemUnderTest()
+    jest.spyOn(hasherStub, 'hash').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
     const promise = systemUnderTest.add(makeFakeAccountData())
     await expect(promise).rejects.toThrow()
   })
@@ -79,7 +79,7 @@ describe('DbAddAccount Usecase', () => {
     })
   })
 
-  test('Should throw if Encrypter throws', async () => {
+  test('Should throw if Hasher throws', async () => {
     const { addAccountRepositoryStub, systemUnderTest } = makeSystemUnderTest()
     jest.spyOn(addAccountRepositoryStub, 'add').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
     const promise = systemUnderTest.add(makeFakeAccountData())
--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -13,20 +13,20 @@ describe('Bcrypt Adapter', () => {
   test('Should call bcrypt with correct values', async () => {
     const systemUnderTest = makeSystemUnderTest()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
-    await systemUnderTest.encrypt('any_value')
+    await systemUnderTest.hash('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
   })
 
   test('Should return a hash on success', async () => {
     const systemUnderTest = makeSystemUnderTest()
-    const hash = await systemUnderTest.encrypt('any_value')
+    const hash = await systemUnderTest.hash('any_value')
     expect(hash).toBe('hash')
   })
 
   test('Should throw if bcrypt throws', async () => {
     const systemUnderTest = makeSystemUnderTest()
     jest.spyOn(bcrypt, 'hash').mockReturnValue(new Promise((resolve, reject) => reject(new Error())))
-    const promise = systemUnderTest.encrypt('any_value')
+    const promise = systemUnderTest.hash('any_value')
     await expect(promise).rejects.toThrow()
   })
 })
