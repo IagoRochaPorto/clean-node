398966b760c48c55b26ecefb0cd4a64e44fa5909
Iago Rocha Porto <iagorochaporto@gmail.com>
2021-03-06 13:42:20 -0300

    test: ensure BcryptAdaper calls bcrypt compare with correct values

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -2,7 +2,8 @@ import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter'
 
 jest.mock('bcrypt', () => ({
-  hash: async (): Promise<string> => new Promise(resolve => resolve('hash'))
+  hash: async (): Promise<string> => new Promise(resolve => resolve('hash')),
+  compare: async (): Promise<boolean> => new Promise(resolve => resolve(true))
 }))
 
 const salt = 12
@@ -10,14 +11,14 @@ const salt = 12
 const makeSystemUnderTest = (): BcryptAdapter => new BcryptAdapter(salt)
 
 describe('Bcrypt Adapter', () => {
-  test('Should call bcrypt with correct values', async () => {
+  test('Should call hash with correct values', async () => {
     const systemUnderTest = makeSystemUnderTest()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await systemUnderTest.hash('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
   })
 
-  test('Should return a hash on success', async () => {
+  test('Should return a valid hash on hash success', async () => {
     const systemUnderTest = makeSystemUnderTest()
     const hash = await systemUnderTest.hash('any_value')
     expect(hash).toBe('hash')
@@ -28,5 +29,12 @@ describe('Bcrypt Adapter', () => {
     jest.spyOn(bcrypt, 'hash').mockReturnValue(new Promise((resolve, reject) => reject(new Error())))
     const promise = systemUnderTest.hash('any_value')
     await expect(promise).rejects.toThrow()
+  })
+
+  test('Should call compare with correct values', async () => {
+    const systemUnderTest = makeSystemUnderTest()
+    const compareSpy = jest.spyOn(bcrypt, 'compare')
+    await systemUnderTest.compare('any_value', 'any_hash')
+    expect(compareSpy).toHaveBeenCalledWith('any_value', 'any_hash')
   })
 })
