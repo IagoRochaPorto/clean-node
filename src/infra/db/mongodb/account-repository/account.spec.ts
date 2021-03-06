5a54d46a6cdaa360374330791046442eac9de943
Iago Rocha Porto <iagorochaporto@gmail.com>
2021-03-06 15:03:30 -0300

    test: ensure AccountMongoRepository returns an account on loadByEmail success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,20 +1,41 @@
+import { Collection } from 'mongodb'
 import { MongoHelper } from '../helpers/mongo-helper'
 import { AccountMongoRepository } from './account'
+
+let accountCollection: Collection
 
 describe('Account Mongo Repository', () => {
   beforeAll(async () => await MongoHelper.connect(process.env.MONGO_URL))
   afterAll(async () => await MongoHelper.disconnect())
-  beforeEach(async () => await (await MongoHelper.getCollection('account')).deleteMany({}))
+  beforeEach(async () => {
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
 
   const makeSystemUnderTest = (): AccountMongoRepository => new AccountMongoRepository()
 
-  test('Should return an account on success', async () => {
+  test('Should return an account on add success', async () => {
     const systemUnderTest = makeSystemUnderTest()
     const account = await systemUnderTest.add({
       name: 'any_name',
       email: 'any_mail@mail.com',
       password: 'any_password'
     })
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_mail@mail.com')
+    expect(account.password).toBe('any_password')
+  })
+
+  test('Should return an account on loadByEmail success', async () => {
+    const systemUnderTest = makeSystemUnderTest()
+    await accountCollection.insertOne({
+      name: 'any_name',
+      email: 'any_mail@mail.com',
+      password: 'any_password'
+    })
+    const account = await systemUnderTest.loadByEmail('any_mail@mail.com')
     expect(account).toBeTruthy()
     expect(account.id).toBeTruthy()
     expect(account.name).toBe('any_name')
