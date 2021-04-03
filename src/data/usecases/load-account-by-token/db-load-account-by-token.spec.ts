963597f53cf8cbcc14219388287d359c219396ee
Iago Rocha Porto <iagorochaporto@gmail.com>
2021-04-03 20:50:44 -0300

    refactor: move SystemUnderTest creation to a factory method

--- src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
@@ -1,16 +1,32 @@
 import { Decrypter } from '../../protocols/criptography/decrypter'
 import { DbLoadAccountByToken } from './db-load-account-by-token'
 
+const makeDecrypter = (): Decrypter => {
+  class DecrypterStub implements Decrypter {
+    async decrypt(value: string): Promise<string> {
+      return new Promise(resolve => resolve('any_value'))
+    }
+  }
+  return new DecrypterStub()
+}
+interface SystemUnderTestTypes {
+  systemUnderTest: DbLoadAccountByToken
+  decrypterStub: Decrypter
+}
+
+const makeSystemUnderTest = (): SystemUnderTestTypes => {
+  const decrypterStub = makeDecrypter()
+  const systemUnderTest = new DbLoadAccountByToken(decrypterStub)
+  return {
+    systemUnderTest,
+    decrypterStub
+  }
+}
+
 describe('DbLoadAccountByToken Usecase', () => {
   test('Should call Decrypter with correct values', async () => {
-    class DecrypterStub implements Decrypter {
-      async decrypt(value: string): Promise<string> {
-        return new Promise(resolve => resolve('any_value'))
-      }
-    }
-    const decrypterStub = new DecrypterStub()
+    const { systemUnderTest, decrypterStub } = makeSystemUnderTest()
     const decryptSpy = jest.spyOn(decrypterStub, 'decrypt')
-    const systemUnderTest = new DbLoadAccountByToken(decrypterStub)
     await systemUnderTest.load('any_token')
     expect(decryptSpy).toHaveBeenCalledWith('any_token')
   })
