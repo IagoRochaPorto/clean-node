91d040b775e6aa8fd9a455c2f599b1821928ea4a
Iago Rocha Porto <iagorochaporto@gmail.com>
2021-04-03 20:07:08 -0300

    refactor: move SystemUnderTest creation to a factory method

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -11,34 +11,44 @@ const makeFakeAccount = (): AccountModel => ({
   password: 'hashed_password'
 })
 
+const makeLoadAccountByToken = (): LoadAccountByToken => {
+  class LoadAccountByTokenStub implements LoadAccountByToken {
+    async load(accessToken: string, role?: string): Promise<AccountModel> {
+      return new Promise(resolve => resolve(makeFakeAccount()))
+    }
+  }
+
+  const loadAccountByTokenStub = new LoadAccountByTokenStub()
+  return loadAccountByTokenStub
+}
+
+interface SystemUnderTestTypes {
+  systemUnderTest: AuthMiddleware
+  loadAccountByTokenStub: LoadAccountByToken
+}
+
+const makeSystemUnderTest = (): SystemUnderTestTypes => {
+  const loadAccountByTokenStub = makeLoadAccountByToken()
+  const systemUnderTest = new AuthMiddleware(loadAccountByTokenStub)
+  return {
+    loadAccountByTokenStub,
+    systemUnderTest
+  }
+}
+
 describe('Auth Middleware', () => {
   test('Should return 403 if no x-acess-token exists in headers', async () => {
-    class LoadAccountByTokenStub implements LoadAccountByToken {
-      async load(accessToken: string, role?: string): Promise<AccountModel> {
-        return new Promise(resolve => resolve(makeFakeAccount()))
-      }
-    }
-
-    const loadAccountByTokenStub = new LoadAccountByTokenStub()
-    const systemUnderTest = new AuthMiddleware(loadAccountByTokenStub)
+    const { systemUnderTest } = makeSystemUnderTest()
     const httpResponse = await systemUnderTest.handle({})
     expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
   })
 
   test('Should call LoadAccountByToken with correct accessToken', async () => {
-    class LoadAccountByTokenStub implements LoadAccountByToken {
-      async load(accessToken: string, role?: string): Promise<AccountModel> {
-        return new Promise(resolve => resolve(makeFakeAccount()))
-      }
-    }
-
-    const loadAccountByTokenStub = new LoadAccountByTokenStub()
+    const { loadAccountByTokenStub, systemUnderTest } = makeSystemUnderTest()
     const loadSpy = jest.spyOn(loadAccountByTokenStub, 'load')
-    const systemUnderTest = new AuthMiddleware(loadAccountByTokenStub)
+
     await systemUnderTest.handle({
-      headers: {
-        'x-access-token': 'any_token'
-      }
+      headers: { 'x-access-token': 'any_token' }
     })
     expect(loadSpy).toHaveBeenLastCalledWith('any_token')
   })
