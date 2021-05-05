a422fbf323fe77faf90823f7c369d5ee9c15911d
Iago Rocha Porto <iagorochaporto@gmail.com>
2021-05-05 14:35:45 -0300

    refactor: organize SureyMongoRepository tests in describes

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -3,6 +3,7 @@ import { MongoHelper } from '../helpers/mongo-helper'
 import { SurveyMongoRepository } from './survey-mongo-repository'
 
 let surveyCollection: Collection
+const makeSystemUnderTest = (): SurveyMongoRepository => new SurveyMongoRepository()
 
 describe('Survey Mongo Repository', () => {
   beforeAll(async () => await MongoHelper.connect(process.env.MONGO_URL))
@@ -12,21 +13,21 @@ describe('Survey Mongo Repository', () => {
     await surveyCollection.deleteMany({})
   })
 
-  const makeSystemUnderTest = (): SurveyMongoRepository => new SurveyMongoRepository()
-
-  test('Should add a survey on success', async () => {
-    const systemUnderTest = makeSystemUnderTest()
-    await systemUnderTest.add({
-      question: 'any_question',
-      answers: [{
-        image: 'any_image',
-        answer: 'any_answer'
-      }, {
-        answer: 'another_answer'
-      }],
-      date: new Date()
+  describe('add()', () => {
+    test('Should add a survey on success', async () => {
+      const systemUnderTest = makeSystemUnderTest()
+      await systemUnderTest.add({
+        question: 'any_question',
+        answers: [{
+          image: 'any_image',
+          answer: 'any_answer'
+        }, {
+          answer: 'another_answer'
+        }],
+        date: new Date()
+      })
+      const survey = await surveyCollection.findOne({ question: 'any_question' })
+      expect(survey).toBeTruthy()
     })
-    const survey = await surveyCollection.findOne({ question: 'any_question' })
-    expect(survey).toBeTruthy()
   })
 })
